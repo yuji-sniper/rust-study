b085327fe51a6fed2c3ea052abd9ae1607edc831
吉村直人 <ahowateto123@gmail.com>
2024-03-29 03:39:48 +0900

    MutableとImmutable

--- app/src/vars.rs
+++ app/src/vars.rs
@@ -1,8 +1,68 @@
 // pub mod sub_a;
 // pub mod sub_b;
 
+// const MAX_POINTS: u32 = 100_000;
+
 pub fn run() {
     println!("Here is vars module!");
+    // モジュール
     // sub_a::func_a();
     // sub_b::func_b();
+
+
+    // 変数
+    // let mut x = 5;
+    // println!("x = {}", x);
+    // x = 6;
+    // println!("x = {}", x);
+
+    // let _i1 = 3;
+    // let _f1 = 3.0;
+
+
+    // ポインタ
+    // println!("{}", usize::BITS);
+    // println!("Memory address of MAX_POINTS: {:p}", &MAX_POINTS);
+
+    // let i2: i64 = 1;
+    // let i3: i64 = 2;
+    // println!("Stack address of i2: {:p}", &i2);
+    // println!("Stack address of i3: {:p}", &i3);
+
+    // let y = 5;
+    // println!("Stack address of y: {:p}", &y);
+    // let y = y + 1;
+    // println!("Stack address of y: {:p}", &y);
+    // let y = y * 2;
+    // println!("Stack address of y: {:p}", &y);
+    // println!("y = {}", y);
+    // {
+    //     let y = 3;
+    //     println!("Stack address of y: {:p}", &y);
+    //     println!("y = {}", y);
+    // }
+    // println!("Stack address of y: {:p}", &y);
+    // println!("y = {}", y);
+
+
+    // タプル
+    // let t1 = (500, 6.4, "Hello");
+    // let (x, y, z) = t1;
+    // println!("x = {}, y = {}, z = {}", x, y, z);
+    // println!("x = {}, y = {}, z = {}", t1.0, t1.1, t1.2);
+
+    // let mut t2 = ((0, 1), (2, 3));
+    // let ((ref mut x1_ptr, ref mut y1_ptr), _) = t2;
+    // *x1_ptr = 5;
+    // *y1_ptr = -5;
+    // println!("t2 = {:?}", t2);
+
+
+    // 配列
+    // let a1 = [1, 2, 3, 4, 5];
+    // let a2 = [0; 10];
+    // println!("a1 = {:?}", a1);
+    // println!("a2 = {:?}", a2);
+    // println!("a1[2] = {}", a1[2]);
+    // println!("a2[3] = {}", a2[3]);
 }
