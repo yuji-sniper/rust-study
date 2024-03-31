3d23eb46b4565cc9520def40c7cf562e7dfaf173
吉村直人 <ahowateto123@gmail.com>
2024-04-01 00:14:02 +0900

    文字列スライス

--- app/src/vars.rs
+++ app/src/vars.rs
@@ -65,4 +65,14 @@ pub fn run() {
     // println!("a2 = {:?}", a2);
     // println!("a1[2] = {}", a1[2]);
     // println!("a2[3] = {}", a2[3]);
+
+    // 文字列スライス
+    let s1 = "Helloこんにちは"; // 20 bytes
+    let s2 = "hello"; // 5 bytes
+    println!("Stack address of s1: {:p}", &s1);
+    println!("Stack address of s2: {:p}", &s2);
+    println!("Static memory address of s1: {:p}", s1.as_ptr());
+    println!("Static memory address of s2: {:p}", s2.as_ptr());
+    println!("Length of s1: {}", s1.len());
+    println!("Length of s2: {}", s2.len());
 }
