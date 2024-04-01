fb6d1f506bf41413d080fd2a63062d2abec8d29b
吉村直人 <ahowateto123@gmail.com>
2024-04-02 08:06:01 +0900

    スタックオーバーフロー

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,10 @@
-mod vars;
+mod stack_heap;
+// mod vars;
 
 fn main() {
     // println!("Hello, world!");
-    vars::run();
+    // vars::run();
+    stack_heap::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
---
+++ app/src/stack_heap.rs
@@ -0,0 +1,4 @@
+pub fn run() {
+    let a1: [u8; 7000000] = [1; 7000000];
+    println!("Length of a1: {}", a1.len());
+}
