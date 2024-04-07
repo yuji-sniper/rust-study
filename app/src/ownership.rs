da46d15cb80193e7078a9731284fe51f939f3607
吉村直人 <ahowateto123@gmail.com>
2024-04-07 23:12:48 +0900

    文字列スライスの参照コピー

--- app/src/ownership.rs
+++ app/src/ownership.rs
@@ -5,10 +5,17 @@ pub fn run() {
     // println!("{} {}", s1, s2); // s1は所有権を持っていないのでエラーになる
     println!("{}", s2); // s2は所有権を持っているので正常に表示される
 
-    // コピー
+    // int型の場合（Copyトレイトを実装している型）
     let i1 = 1;
     let i2 = i1; // コピーされる
     println!("{} {}", i1, i2); // アドレスは異なるが値は同じ
     println!("Stack address of i1: {:p}", &i1);
     println!("Stack address of i2: {:p}", &i2);
+
+    // 文字列スライス（これもCopyトレイトを実装している型）。参照をコピーしている。
+    let sl1 = "hello";
+    let sl2 = sl1;
+    println!("{} {}", sl1, sl2); // アドレスは異なるが値は同じ
+    println!("Stack address of sl1: {:p}", &sl1);
+    println!("Stack address of sl2: {:p}", &sl2);
 }
