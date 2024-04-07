d583fa873b77de35bd1c6b8cf5df92f9642ea6e9
吉村直人 <ahowateto123@gmail.com>
2024-04-07 20:51:04 +0900

    ownershopモジュール作成

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,12 @@
-mod stack_heap;
+mod ownership;
+// mod stack_heap;
 // mod vars;
 
 fn main() {
     // println!("Hello, world!");
+    ownership::run();
+    // stack_heap::run();
     // vars::run();
-    stack_heap::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
